b4872453af032260870ba48ae39bfdd14c0c64a0
EkaterinaZi <kat.zinovik@gmail.com>
2022-07-28 22:22:41 +0100

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const body = document.querySelector('body');
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  let timerId = null;
+  startBtn.addEventListener('click', startChangeColor);
+  stopBtn.addEventListener('click', stopChangeColor);
+
+
+function startChangeColor(){
+    timerId = setInterval(changeColour, 1000);
+    startBtn.setAttribute('disabled', 'disabled');
+}
+
+function changeColour(){
+body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopChangeColor(){
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+}
